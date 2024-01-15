a938fbb22385feecbe8ffed340232ef56eeb13a0
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-15 08:47:15 +0100

    Feat JS - ticket progress

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,56 @@
 /* COLLEGAMENTI */
 
-/* const userName = document.getElementById("username"); */
+const userName = document.getElementById("username");
 const routeLength = document.getElementById("routeLength");
 const userAge = document.getElementById("userAge");
 const submitData = document.getElementById("submitData");
-const ticketPrice = document.getElementById("ticket");
+
+const ticketUserName = document.getElementById("ticketUserName");
+const ticketDiscount = document.getElementById("ticketDiscount");
+const ticketCarriage = document.getElementById("ticketCarriage");
+const ticketLabelPrice = document.getElementById("ticketPrice");
 
 /* AZIONI  */
 
 submitData.addEventListener("click", function () {
+  const passengerName = userName.value;
   const distance = parseInt(routeLength.value);
   const age = parseInt(userAge.value);
+  const trainCarriage = Math.floor(Math.random() * 8 + 1);
 
   const priceOnlyByDistance = distance * 0.21;
 
   let discount;
+  let discountText;
   if (!isNaN(distance) && !isNaN(age)) {
     /* Calculate discounts */
     /*!!!!! Underage discount */
     if (age < 18) {
       discount = (priceOnlyByDistance / 100) * 20;
-      console.log(discount);
-    } else if (age >= 18) {
-      /*!!!!! NO discount */
-      discount = (priceOnlyByDistance / 100) * 0;
-      console.log(discount);
+      discountText = "Under 18";
     } else if (age > 65) {
-      /*  !!!!! OVER65 discount */
+      /*!!!!! NO discount */
       discount = (priceOnlyByDistance / 100) * 60;
-      console.log(discount);
+      discountText = "Adulto";
+    } else if (age >= 18) {
+      /*  !!!!! OVER65 discount */
+      discount = (priceOnlyByDistance / 100) * 0;
+      discountText = "Over65";
     }
 
     /* Final price */
     const finalPrice = priceOnlyByDistance - discount;
+    const ticketPrice = finalPrice.toFixed(2) + "€";
 
     /* Print final price */
-    const labelPrice =
-      "Il tuo biglietto del treno costerà: " + finalPrice.toFixed(2) + "€";
-    ticket.innerText = labelPrice;
+
+    ticketUserName.innerText = passengerName;
+    ticketDiscount.innerText = discountText;
+    ticketCarriage.innerText = trainCarriage;
+    ticketLabelPrice.innerText = ticketPrice;
   } else {
     alert(
-      "Dati inseriti non corretti, inserire km con SOLO tasti numerici ed età SOLO con tasti numerici. Ricarica la pagina."
+      "Dati inseriti non corretti, inserire km con SOLO tasti numerici ed età SOLO con tasti numerici."
     );
   }
 });
